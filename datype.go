48a4e9759db2fbfe70f371195a55012fac64133b
changhorang <rlackdgh124@gmail.com>
2024-06-01 17:42:44 +0900

    [240601] data type return

--- datype.go
+++ datype.go
@@ -1,6 +1,9 @@
 package exam
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func ConVar() {
 	const name string = "kim"
@@ -12,4 +15,18 @@ func ConVar() {
 	name3 := "kang" // GO 에서 data type 설정 (variable)
 	name3 = "kkang"
 	fmt.Println(name3)
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+// return data type 지정
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+// many arguments
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
