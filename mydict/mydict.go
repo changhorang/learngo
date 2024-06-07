72d2156e2f7ea2d60d1b779911661886b8c1141f
changhorang <rlackdgh124@gmail.com>
2024-06-07 12:32:54 +0900

    [240607] Dict Method - Add - Update - Delete

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,7 +5,11 @@ import "errors"
 // Dictionary
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
+var (
+	errNotFound   = errors.New("Not Found")
+	errWordExists = errors.New("Word Exists")
+	errCantUpdate = errors.New("Can't update non-existing word")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +18,34 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word) // delete return anything
 }
