76aa2302c21911ec56571ba625b52ad71087026c
changhorang <rlackdgh124@gmail.com>
2024-06-08 16:45:27 +0900

    [240608] jobscrapper - 사람인 구인 스크랩 - goquery > go mod init ~~~ > go get github.com...... - string trim / join / fields - http get - struct append

---
+++ jobscrapper/jobscrpper.go
@@ -0,0 +1,99 @@
+package main // src에서 main.go로 실행 가능
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://saramin.co.kr/zf_user/search/recruit?searchword=python"
+
+type extractedJob struct {
+	id            string
+	title         string
+	job_condition string
+	job_date      string
+	job_sector    string
+}
+
+func main() {
+	var jobs []extractedJob
+	totalPages := getPages()
+	for i := 1; i < totalPages; i++ {
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
+	}
+	fmt.Println(jobs)
+}
+
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searcItems := doc.Find(".item_recruit")
+
+	searcItems.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+
+	})
+	return jobs
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode)
+	}
+}
+
+func cleeaString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("value")
+	title := cleeaString(card.Find(".job_tit>a").Text())
+	job_condition := cleeaString(card.Find(".job_condition").Text())
+	job_date := cleeaString(card.Find(".job_date").Text())
+	job_sector := cleeaString(card.Find(".job_sector").Text())
+	fmt.Println(id, title, job_condition, job_date, job_sector)
+	return extractedJob{
+		id:            id,
+		title:         title,
+		job_condition: job_condition,
+		job_date:      job_date,
+		job_sector:    job_sector,
+	}
+}
